3dc666998bce89d52277464fa75f42b21071baa0
Dita Nurhalimah <ditanurhalimah@gmail.com>
2023-03-10 04:01:05 +0700

    add tailwind config

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div class="bg-white p-6">
     <h1 class="text-3xl font-bold mb-5">List Games</h1>
     <div v-if="loading" class="text-center">Loading...</div>
     <div v-else>
       <ul>
-        <li v-for="game in games" :key="game.id">
-          <nuxt-link :to="`/game/${game.id}`">{{ game.title }}</nuxt-link>
+        <li v-for="game in games" :key="game.id" class="mb-2">
+          <nuxt-link :to="`/game/${game.id}`" class="text-blue-600 hover:text-blue-800">{{ game.title }}</nuxt-link>
         </li>
       </ul>
     </div>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './pages/**/*.vue',
+    './components/**/*.vue',
+    './layouts/**/*.vue',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
