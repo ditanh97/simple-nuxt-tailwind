84532e9fc62fbf1566c0f65d33707670f1d0d6cb
Dita Nurhalimah <ditanurhalimah@gmail.com>
2023-03-10 03:39:59 +0700

    first commit

---
+++ pages/game/_id.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1 class="text-3xl font-bold mb-5">{{ game.title }}</h1>
+      <div v-if="loading" class="text-center">Loading...</div>
+      <div v-else>
+        <p>{{ game.short_description }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    async asyncData({ $axios, params }) {
+      const response = await $axios.get(`https://www.mmobomb.com/api1/game?id=${params.id}`)
+      return {
+        game: response.data,
+        loading: false
+      }
+    },
+  }
+  </script>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold mb-5">List Games</h1>
+    <div v-if="loading" class="text-center">Loading...</div>
+    <div v-else>
+      <ul>
+        <li v-for="game in games" :key="game.id">
+          <nuxt-link :to="`/game/${game.id}`">{{ game.title }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    async asyncData({ $axios }) {
+      const response = await $axios.get('https://www.mmobomb.com/api1/games?platform=pc')
+      return {
+        games: response.data,
+        loading: false
+      }
+    },
+  }
+</script>
